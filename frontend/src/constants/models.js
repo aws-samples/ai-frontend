cfc547d49d36be7506d5a5b10d2deccbdbe744de
Andrew Young <andrewjunyoung1@gmail.com>
2024-11-25 16:50:26 -0500

    Fixed haiku and Opus. Leaving Opus in even though LLM Gateway doesn't give me those perms,because it shows the gateway working!

--- frontend/src/constants/models.js
+++ frontend/src/constants/models.js
@@ -1,6 +1,6 @@
 export const MODELS = {
   // Anthropic.
-  HAIKU: "anthropic.claude-3-haiku-20240229-v1:0",
+  HAIKU: "anthropic.claude-3-haiku-20240307-v1:0",
   SONNET: "anthropic.claude-3-sonnet-20240229-v1:0",
   OPUS: "anthropic.claude-3-opus-20240229-v1:0",
   // Amazon.
